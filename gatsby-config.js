58c63ca398e4e5f41d51d56ed15019909d954bd5
hersh-kat <h.kataria@warwick.ac.uk>
2020-09-05 19:03:36 +0100

    Updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Hersh Kataria's Personal Website`,
+    siteUrl: `https://www.hershkat.com`,
+    description: `Welcome to my home on the internet, where you can get to know who I am and what I do!`,
+  },
   plugins: [],
-}
+};
